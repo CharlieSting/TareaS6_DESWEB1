819cec4852eeab1e06a70dc661c95061874e9736
CharlieSting <101151950+CharlieSting@users.noreply.github.com>
2023-02-23 19:16:42 -0600

    comit

---
+++ CalculadoraS6/CalculadoraS6/CalculadoraS6/Models/Expresion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace CalculadoraS6.Models
+{
+    public class Expresion
+    {
+        // (num1 y num2) y los métodos (Sumar, Restar, Multiplicar, Dividir)
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+        public  double Sumar()
+        {
+            double total;
+            total= Num1 + Num2;
+            return total;
+        }
+        public double Restar()
+        {
+            double total;
+            total = Num1 - Num2;
+            return total;
+        }
+
+        public double Multiplicar()
+        {
+                double total;
+                total = Num1 * Num2;
+                return total;
+        }
+        public double Dividir() {
+            double total;
+            total = Num1 / Num2;
+            return total;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Resultado: ";
+        }
+
+
+    }
+}
---
+++ CalculadoraS6/CalculadoraS6/CalculadoraS6/ViewModel/ViewModelExpresion.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using CalculadoraS6.Models;
+
+namespace CalculadoraS6.ViewModel
+{
+    public class ViewModelExpresion : INotifyPropertyChanged
+    {
+
+        public ViewModelExpresion(){
+
+            Sumar = new Command(() =>
+            {
+
+                Expresion p = new Expresion()
+                {
+                    Num1 = this.num2,
+                    Num2 = this.num2
+                   
+                };
+
+                Total = p.Sumar();
+
+
+
+            });
+
+            Restar = new Command(() =>
+            {
+
+                Expresion p = new Expresion()
+                {
+                    Num1 = this.num2,
+                    Num2 = this.num2,
+
+
+
+                };
+
+                Total = p.Restar();
+
+
+
+            });
+
+            Multiplicar = new Command(() =>
+            {
+
+                Expresion p = new Expresion()
+                {
+                    Num1 = this.num2,
+                    Num2 = this.num2
+
+                };
+
+                Total = p.Multiplicar();
+
+
+
+            });
+
+            Dividir = new Command(() =>
+            {
+
+                Expresion p = new Expresion()
+                {
+                    Num1 = this.num2,
+                    Num2 = this.num2
+
+                };
+
+                Total = p.Dividir();
+
+
+
+            });
+        }
+
+
+            double num1;
+
+        public double Num1
+        {
+
+            get => num1;
+            set
+            {
+
+                num1 = value;
+                var args = new PropertyChangedEventArgs(nameof(Num1));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+        double num2;
+
+        public double Num2
+        {
+
+            get => num2;
+            set
+            {
+
+                num1 = value;
+                var args = new PropertyChangedEventArgs(nameof(Num2));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+        double total;
+        public double Total
+        {
+
+            get => total;
+            set
+            {
+                total = value;
+                var args = new PropertyChangedEventArgs(nameof(Total));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+        public Command Sumar { get; }
+        public Command Restar { get; }
+        public Command Multiplicar { get; }
+        public Command Dividir { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
